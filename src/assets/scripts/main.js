813d3fac858691abf5fe48e4c9a7d48060285a0e
jorditosa <jordisato88@gmail.com>
2023-05-24 16:49:16 +0200

    dinamic header

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -7,7 +7,18 @@
  * Write any other JavaScript below
  */
 
-/**
- * Home - Cards section
- */
+  // Obtener la URL actual
+  const currentURL = window.location.href;
+  let lastPart = currentURL.substring(currentURL.lastIndexOf("/") + 1);
 
+  // Obtener el elemento <nav> por su ID
+  let header = document.getElementById("header");
+
+  // Comprobar si la URL contiene "about.html"
+  if (lastPart.includes("about.html")) {
+    // Si la URL contiene "about.html", establecer el fondo blanco
+    header.style.backgroundColor = "#ffffff";
+  } else {
+    // Si la URL no contiene "about.html", establecer el fondo gris
+    header.style.backgroundColor = "#dee2e6";
+  }
